9415eca6626737956538a82dc0c9d12ce2f46baa
Saiful Islam <saiful.abir20@gmail.com>
2024-05-30 11:43:25 +0200

    filter added to feature list api

--- facility/api.py
+++ facility/api.py
@@ -7,13 +7,28 @@ from facility.serializers import SchoolListSerializer, YouthCareerListSerializer
 class FeatureListAPIView(APIView):
 
     def get(self, request):
-        schools = School.objects.all()
-        social_works = SocialWork.objects.all()
-        youth_careers = YouthCareer.objects.all()
-        daycares = Daycare.objects.all()
-        school_serializer = SchoolListSerializer(schools, many=True)
-        social_work_serializer = SchoolListSerializer(social_works, many=True)
-        youth_career_serializer = YouthCareerListSerializer(youth_careers, many=True)
-        daycare_serializer = DaycareListSerializer(daycares, many=True)
-        return Response({"school_list": school_serializer.data, "social_work_list": social_work_serializer.data,
-                         "youth_career_list": youth_career_serializer.data, "daycare_list": daycare_serializer.data})
+        feature_dict = {"school_list": None, "social_work_list": None, "youth_career_list": None, "daycare_list": None}
+        category_list = request.GET.getlist("category")
+
+        for category in category_list:
+            if category == "Schulen":
+                schools = School.objects.all()
+                school_serializer = SchoolListSerializer(schools, many=True)
+                feature_dict["school_list"] = school_serializer.data
+
+            if category == "Kindertageseinrichtungen":
+                daycares = Daycare.objects.all()
+                daycare_serializer = DaycareListSerializer(daycares, many=True)
+                feature_dict["daycare_list"] = daycare_serializer.data
+
+            if category == "Schulsozialarbeit":
+                social_works = SocialWork.objects.all()
+                social_work_serializer = SchoolListSerializer(social_works, many=True)
+                feature_dict["social_work_list"] = social_work_serializer.data
+
+            if category == "Jugendberufshilfen":
+                youth_careers = YouthCareer.objects.all()
+                youth_career_serializer = YouthCareerListSerializer(youth_careers, many=True)
+                feature_dict["youth_career_list"] = youth_career_serializer.data
+
+        return Response(feature_dict)
