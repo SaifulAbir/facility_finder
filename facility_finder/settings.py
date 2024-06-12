458f8d73e5297e645dc8e40e0a74176d7ead7b74
Saiful Islam <saiful.abir20@gmail.com>
2024-06-12 21:07:16 +0200

    settings updated

--- facility_finder/settings.py
+++ facility_finder/settings.py
@@ -83,16 +83,6 @@ WSGI_APPLICATION = 'facility_finder.wsgi.application'
 CORS_ORIGIN_ALLOW_ALL = True
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
